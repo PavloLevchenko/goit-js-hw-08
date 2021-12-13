5c44ea735b2d53fc4084367a5845c47fd4376d9b
PavloLevchenko <teasetrnet@gmail.com>
2021-12-13 22:37:09 +0200

    Добавляет задание 1

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,28 @@
+import SimpleLightbox from 'simplelightbox';
+// Дополнительный импорт стилей
+import 'simplelightbox/dist/simple-lightbox.min.css';
 // Add imports above this line
 import { galleryItems } from './gallery-items';
 // Change code below this line
 
-console.log(galleryItems);
+let insertHTML = '';
+for (const image of galleryItems) {
+  insertHTML += `<div class="gallery__item">
+			<a href="${image.original}" class="gallery__link">
+				<image class="gallery__image" src="${image.preview}" alt="${image.description}">
+			</a>
+		</div>`;
+}
+
+const gallery = document.querySelector('.gallery');
+gallery.insertAdjacentHTML('beforeend', `${insertHTML}`);
+
+const lightbox = new SimpleLightbox('.gallery .gallery__link', {
+  captions: true,
+  captionSelector: 'img',
+  captionType: 'attr',
+  captionsData: 'alt',
+  captionPosition: 'bottom',
+  captionDelay: 250,
+  captionClass: 'gallery__caption',
+});
