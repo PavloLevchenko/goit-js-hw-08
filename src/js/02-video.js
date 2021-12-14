3da4a3a0fe8cb992ed19d51a80752fe46017bb7a
PavloLevchenko <teasetrnet@gmail.com>
2021-12-14 20:33:01 +0200

    Добавляет второе задание

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,21 @@
+import Player from '@vimeo/player';
+import throttle from '@lodash/throttle';
 
+const iframe = document.querySelector('#vimeo-player');
+const player = new Player(iframe);
+
+player.setCurrentTime(localStorage.getItem('videoplayer-current-time'));
+
+var throttled = _.throttle(setCurrentTime, 1000, { trailing: false });
+
+player.on('play', throttled);
+
+function setCurrentTime() {
+  console.log('played the video!');
+  localStorage.setItem('videoplayer-current-time', data.timeupdate.seconds);
+  console.log(data.timeupdate.seconds);
+}
+
+player.getVideoTitle().then(function (title) {
+  console.log('title:', title);
+});
